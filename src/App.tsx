f2a433808065d2481cb053887dea22026a0e0bc3
Jorge Rama Krsna Mandoju <jorgezin@gmail.com>
2020-02-15 20:05:58 -0300

    Adição do componente da página que possui as informações dos planetas

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import { Header } from './components/organism/Header';
+import { RandomPlanetPage } from './pages/RandomPlanetPage';
 
 function App() {
   return (
-    <div>
+    <div className={"bg-dark"}>
       <Header />
+      <RandomPlanetPage />
     </div>
   );
 }
---
+++ src/pages/RandomPlanetPage.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import style from "./style.module.css";
+
+
+/**
+ * Esse componente renderiza a página que irá mostrar os planetas aleatórios
+ */
+export const RandomPlanetPage = () => {
+
+  return (
+    <div className={style.pageContainer}>
+    </div>
+  );
+};
+
