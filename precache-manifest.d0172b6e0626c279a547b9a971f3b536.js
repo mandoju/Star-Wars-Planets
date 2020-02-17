69c7cf7a448c3bb238eb90a1e144ead58d3a9137
Jorge Rama Krsna Mandoju <jorgezin@gmail.com>
2020-02-16 22:43:40 -0300

    Updates

---
+++ precache-manifest.d0172b6e0626c279a547b9a971f3b536.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6374809e4680101d7e2793bcce1ef35e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d5856c9e110c1e47b7c5",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "9853085c4c28e79cd15d",
+    "url": "/static/css/main.1eb82482.chunk.css"
+  },
+  {
+    "revision": "d5856c9e110c1e47b7c5",
+    "url": "/static/js/2.3405b939.chunk.js"
+  },
+  {
+    "revision": "109393d43508fe3ea6617266d4524804",
+    "url": "/static/js/2.3405b939.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9853085c4c28e79cd15d",
+    "url": "/static/js/main.aff7cce2.chunk.js"
+  },
+  {
+    "revision": "45791d58686925fa3c4c",
+    "url": "/static/js/runtime-main.971b8fbf.js"
+  },
+  {
+    "revision": "11eabca2251325cfc5589c9c6fb57b46",
+    "url": "/static/media/RobotoRegular.11eabca2.ttf"
+  },
+  {
+    "revision": "cc3019aa6439ef893839b8a0d6d0391d",
+    "url": "/static/media/Starjedi.cc3019aa.ttf"
+  }
+]);
